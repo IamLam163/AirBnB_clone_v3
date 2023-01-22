486279407b6b709afd39f1408d8871c3d2c9c41f
IamLam163 <olamidealiyu@gmail.com>
2023-01-22 06:59:22 +0100

    task 6 till delete

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,3 +3,34 @@
 default RESTFul API actions
 '''
 
+from api.v1.views import app_views
+from flask import jsonify, abort
+from models import storage
+from models.state import State
+
+@app_views.route('/states', strict_slashes=False)
+def list_states():
+    """This method returns a list of all States"""
+    states = storage.all(State)
+    return jsonify(
+            [state.to_dict() for state in states.values()]
+            )
+
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def by_states_id(state_id):
+    """Retrieves states objects by ID"""
+    states = storage.get(State, state_id)
+    if states is None:
+        abort(404)
+    return jsonify(
+            states.to_dict()
+            )
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=True)
+def del_states(state_id):
+    """Method deletes state objects"""
+    states = storage.get(State, state_id)
+    if states is None:
+        abort(404)
+    states.delete()
+    return jsonify({}), 200
+
